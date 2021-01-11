f63ef98876e7c957d81adecf062eedc3736416cb
Victor <nguyentanthang9a4@gmail.com>
2021-01-11 16:27:33 +0700

    Version 1.0.1 - Config the navbar interaction and mobile display of carousel

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 $(function () {
+    const navbar = document.querySelector(".navbar");
+    const navbarTogglerButton = document.querySelector(".navbar-toggler");
+
     $(document).scroll(function () {
         var $nav = $(".navbar");
-        $nav.toggleClass('active', $(this).scrollTop() > $nav.height());
+        let isNavbarExpanded = navbarTogglerButton.getAttribute("aria-expanded");
+        if (isNavbarExpanded == "false") {
+            $nav.toggleClass('active', $(this).scrollTop() > $nav.height());
+        } 
     });
+    const navbarToggle = document.querySelector(".fas.fa-bars");
+
+    navbarToggle.addEventListener("click", (e) => {
+        var $nav = $(".navbar");
+        let isNavbarExpanded = navbarTogglerButton.getAttribute("aria-expanded");
+        console.log(isNavbarExpanded);
+        if (isNavbarExpanded == "false") {
+            navbar.classList.add('active');
+        } else {
+            if ($(this).scrollTop() > $nav.height() === false) {
+                navbar.classList.remove('active');
+            }
+        }
+    })
 });
+
